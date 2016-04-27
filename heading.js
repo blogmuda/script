e9cff4ee699b9de57ab9912914816a681fe83c3f
blogmuda <blogmuda.com@gmail.com>
2016-04-28 00:48:36 +0700

    Create heading.js

---
+++ heading.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.blogmuda.com/") {
+        window.location.href = "http://www.blogmuda.com"; }});
+//]]>
+</script>
